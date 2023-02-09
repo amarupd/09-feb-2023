04f034906ba47bdac348b7317e8bbd6b37e70139
amar dutt upadhyay <amarduttupadhyay@gmail.com>
2023-02-09 17:29:38 +0530

    commit

--- app.js
+++ app.js
@@ -16,35 +16,6 @@ app.post("/signup",(req,res)=>{
     res.send('signed up succesfully');
     console.log(value);
 })
-const axios = require("axios");
-
-
-
-    const options = {
-      method: 'POST',
-      url: 'https://nexmo-nexmo-sms-verify-v1.p.rapidapi.com/send-verification-code',
-      params: {phoneNumber: '+917272096364', brand: 'myApp.com'},
-      headers: api
-    };
-    
-    axios.request(options).then(function (response) {
-        console.log(response.data);
-    }).catch(function (error) {
-        console.error(error);
-    });
-
-// app.post("/otp",(req,res)=>{
-//     const phoneNumber=req.body.phoneNumber
-//     const options = {
-//         method: 'POST',
-//         url: 'https://nexmo-nexmo-sms-verify-v1.p.rapidapi.com/send-verification-code',
-//         body: {phoneNumber: '7272096364', brand: 'myApp.com'},
-//         headers: {
-//           'X-RapidAPI-Key': '2d584490d7msh9f933009904afcfp1592a7jsn4568e2eef33c',
-//           'X-RapidAPI-Host': 'nexmo-nexmo-sms-verify-v1.p.rapidapi.com'
-//         }
-//       };
-// })
 
 axios.request(options).then(function (response) {
 	console.log(response.data);
