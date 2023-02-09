4bbd76ee2acc478a628038a6178720d93d07b83f
amar dutt upadhyay <amarduttupadhyay@gmail.com>
2023-02-09 14:56:36 +0530

    node

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ const port=process.env.port || 5300
 
 app.use(express.json())
 
+app.post("/signup",(req,res)=>{
+
+})
+
+app.listen(port,()=>{
+    console.log(`listening to port number ${port} goto http://localhost:${port}/signup`);
+})
